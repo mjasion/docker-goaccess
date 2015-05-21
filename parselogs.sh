5a2b873caa9a762d6b49f68ccc555f034f0702ea
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-21 16:49:22 +0200

    calculating last week logs

--- parselogs.sh
+++ parselogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm /var/files/*.html
-for file in `ls /var/log/nginx/*.log`; do
+for file in `ls /var/log/nginx/*.log /var/log/nginx/*.log.1`; do
   filename=$(basename "$file")
   docker run -v /var/log/nginx:/logs goaccess:latest goaccess --no-progress -f /logs/$filename -a > /var/files/"$filename".html
 done
