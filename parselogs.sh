88999db3e54463738c41e1a0f0b2dde244fe0430
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-21 15:22:44 +0200

    removing old files

--- parselogs.sh
+++ parselogs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm /var/files/*.html
 for file in `ls /var/log/nginx/*.log`; do
   filename=$(basename "$file")
   docker run -v /var/log/nginx:/logs goaccess:latest goaccess --no-progress -f /logs/$filename -a > /var/files/"$filename".html
