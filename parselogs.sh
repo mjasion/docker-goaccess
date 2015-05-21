4e0451ae85fca28981a1a1e3e6dfa7485bc780ee
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-21 13:23:10 +0200

    parselogs

---
+++ parselogs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for file in `ls /var/log/nginx/*.log`; do
+  filename=$(basename "$file")
+  docker run -v /var/log/nginx:/logs goaccess:latest goaccess --no-progress -f /logs/$filename -a > /var/files/"$filename".html
+done
