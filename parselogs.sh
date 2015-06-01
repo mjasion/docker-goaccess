9f2d3e5429b886ed59c11297234bc168287fe3f8
Marcin Jasion <marcin.jasion@gmail.com>
2015-06-01 15:19:12 +0200

    generating status from all historical logs

--- parselogs.sh
+++ parselogs.sh
@@ -7,7 +7,11 @@ for file in `ls /var/log/nginx/*.log /var/log/nginx/*.log.1`; do
 done
 
 cat /var/log/nginx/*.log > /tmp/all_logs.log
-docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/all_logs.html
+docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/this_week.html
 cat /var/log/nginx/*.log.1 > /tmp/all_logs.log
-docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/all_logs.1.html
+docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/last_week.html
+
+cat /var/log/nginx/*.log.1 /var/log/nginx/*.log > /tmp/all_logs.log
+zcat /var/log/nginx/*.gz >> /tmp/all_logs.log
+docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/all_time.html
 rm /tmp/all_logs.log
