db28e3e480a9f5dc9f67cebda1cc2590a37adf6c
Marcin Jasion <marcin.jasion@gmail.com>
2015-06-10 22:50:42 +0200

    parse logs changed function

--- parselogs.sh
+++ parselogs.sh
@@ -1,17 +1,114 @@
 #!/bin/bash
 
-rm /var/files/*.html
-for file in `ls /var/log/nginx/*.log /var/log/nginx/*.log.1`; do
-  filename=$(basename "$file")
-  docker run -v /var/log/nginx:/logs goaccess:latest goaccess --no-progress -f /logs/$filename -a > /var/files/"$filename".html
-done
+cd /var/log/nginx
+HTML_PATH="/var/files/"
+rm $HTML_PATH/*.html
 
-cat /var/log/nginx/*.log > /tmp/all_logs.log
-docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/this_week.html
-cat /var/log/nginx/*.log.1 > /tmp/all_logs.log
-docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/last_week.html
+function pase_all_logs() {
+  for file in `find *.log -size +0`; do
+    docker run -v /var/log/nginx:/logs goaccess:latest goaccess --no-progress -f /logs/$file -a > $HTML_PATH/"$file".html
+  done
+}
 
-cat /var/log/nginx/*.log.1 /var/log/nginx/*.log > /tmp/all_logs.log
-zcat /var/log/nginx/*.gz >> /tmp/all_logs.log
-docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/all_logs.log -a > /var/files/all_time.html
-rm /tmp/all_logs.log
+function cat_all() {
+  for file in $1; do
+    cat $file
+  done
+}
+
+function zcat_all() {
+  for file in $1; do
+    zcat $file
+  done
+}
+
+function goacces() {
+  docker run -v /tmp:/logs goaccess:latest goaccess --no-progress -f /logs/$1 -a
+}
+
+function parse_today_production() {
+  FILE_NAME="today"
+  cat_all "`ls *.log | grep -vi staging`" | grep "`date +"%d/%b/%Y" --date="today"`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_today_staging() {
+  FILE_NAME="staging_today"
+  cat_all "`ls *.log | grep -i staging`" | grep "`date +"%d/%b/%Y" --date="today"`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_yesterday_production() {
+  FILE_NAME="yesterday"
+  cat_all "`ls *.log | grep -vi staging`" | grep "`date +"%d/%b/%Y" --date="yesterday"`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_yesterday_staging() {
+  FILE_NAME="staging_yesterday"
+  cat_all "`ls *.log | grep -i staging`" | grep "`date +"%d/%b/%Y" --date="yesterday"`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_thisweek_production() {
+  FILE_NAME="thisweek"
+  cat_all "`ls *.log | grep -vi staging`"  >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_thisweek_staging() {
+  FILE_NAME="staging_thisweek"
+  cat_all "`ls *.log | grep -i staging`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_lastweek_production() {
+  FILE_NAME="lastweek"
+  cat_all "`ls *.log.1 | grep -vi staging`"  >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function parse_lastweek_staging() {
+  FILE_NAME="staging_lastweek"
+  cat_all "`ls *.log.1 | grep -i staging`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function total_production() {
+  FILE_NAME="total"
+  cat_all "`ls *.log *.log.1 | grep -vi staging`" >> /tmp/$FILE_NAME.log
+  zcat_all "`ls *.gz | grep -vi staging`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+function total_staging() {
+  FILE_NAME="staging_total"
+  cat_all "`ls *.log *.log.1 | grep -i staging`" >> /tmp/$FILE_NAME.log
+  zcat_all "`ls *.gz | grep -i staging`" >> /tmp/$FILE_NAME.log
+  goacces "$FILE_NAME.log" >> $HTML_PATH/$FILE_NAME.html
+  rm /tmp/$FILE_NAME.log
+}
+
+
+pase_all_logs
+
+parse_today_production
+parse_today_staging
+
+parse_yesterday_production
+parse_yesterday_staging
+
+parse_thisweek_production
+parse_thisweek_staging
+
+total_production
+total_staging
